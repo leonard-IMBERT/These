bdf44a3ec447aa4b8f9d20a5d3f5a67d85410e21
Leonard Imbert <imbert@subatech.in2p3.fr>
2025-01-21 15:30:29 +0100

    [WIP] Dernire corrections

--- generate_report.sh
+++ generate_report.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
-TEXTIDOTE_PATH=~/Downloads/textidote.jar-0.9.jar
+TEXTIDOTE_PATH=~/Software/textidote.jar
 
 mkdir -p reports
 
 
+echo $([[ "$1" -eq "-f" ]])
 echo "Generating reports"
 
 for file in chapters/*; do
   chapter_name=$(basename $file)
-  if [[ chapters/$chapter_name -nt reports/$chapter_name.html ]] || [[ reports/dico.txt -nt reports/$chapter_name.html ]]; then
+  if [[ chapters/$chapter_name -nt reports/$chapter_name.html ]] || [[ reports/dico.txt -nt reports/$chapter_name.html ]] || [[ "$1" == "-f" ]]; then
     echo "Generating report for $chapter_name"
-    java -jar $TEXTIDOTE_PATH --check en \
+    lang="en"
+    if [[ $chapter_name == "remerciements.tex" ]] || [[ $chapter_name == "resume.tex" ]]; then
+      lang="fr"
+    fi
+    java -jar $TEXTIDOTE_PATH --check $lang \
       --dict reports/dico.txt \
       --output html \
-      --ignore sh:c:noin,sh:figref,lt:en:SPACEX \
+      --ignore sh:seclen,sh:capperiod,sh:c:noin,sh:figref,lt:en:SPACEX \
       chapters/$chapter_name > reports/$chapter_name.html
   else
     echo "$chapter_name is up to date"
